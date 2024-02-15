076152ddd5f0188f01c0dcb51f4f432fe54509f3
Hung Ruo Han <hungrh@gmail.com>
2024-02-15 18:55:18 -0300

    Removed main file

--- src/main.py
+++
@@ -1,8 +0,0 @@
-from pandas_datareader import data as pdr
-import yfinance as yfin
-
-yfin.pdr_override()
-
-petr4_df = pdr.get_data_yahoo('PETR4.SA', start='2022-10-24', end='2022-12-23')
-print("petr4_df.head: {}".format(petr4_df.head()))
-print("petr4_df.tail: {}".format(petr4_df.tail()))
