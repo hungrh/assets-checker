c2d5dfba395b69e4e264bce7d000042feb509110
Hung Ruo Han <hungrh@gmail.com>
2024-02-16 18:00:45 -0300

    Added GOLD index to correlation example.

--- examples/assets-correlation.py
+++ examples/assets-correlation.py
@@ -6,20 +6,20 @@ import seaborn
 from yahooquery import Ticker
 
 symbols_list = [
-    "IVVB11.SA",
-    "BITH11.SA",
-    "BOVA11.SA",
+    "^BVSP", # IBOVESPA
     "^GSPC", # S&P 500
     "^FVX", # Treasury Yield 5 Years
     "^TYX", # Treasury Yield 30 Years
     "BTC-USD",
+    "BITH11.SA",
+    "GOLD11.SA",
 ]
 
 tickers = Ticker(" ".join(symbols_list))
-df = tickers.history()
+df = tickers.history(period='max')
 df = df.reset_index()
-df = df[['date', 'close', 'symbol']]
-df_pivot=df.pivot(index='date', columns='symbol', values='close').reset_index(drop=True)
+df = df[['date', 'adjclose', 'symbol']]
+df_pivot=df.pivot(index='date', columns='symbol', values='adjclose').reset_index(drop=True)
 
 corr_df = df_pivot.corr(method='pearson')
 
